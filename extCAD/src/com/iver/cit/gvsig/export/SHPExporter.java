a8e8da130f2a4f71d48782750496a5cec768d75b
Francisco Puga <fpuga@icarto.es>
2018-01-29 14:19:45 +0100

    Small refactor to delegate some commont methods on icarto-commons
    extCAD
    refs #984

--- extCAD/src/com/iver/cit/gvsig/export/SHPExporter.java
+++ extCAD/src/com/iver/cit/gvsig/export/SHPExporter.java
@@ -31,6 +31,9 @@ import com.iver.cit.gvsig.fmap.layers.SelectableDataSource;
 import com.iver.cit.gvsig.fmap.layers.VectorialFileAdapter;
 import com.iver.utiles.SimpleFileFilter;
 
+import es.icarto.gvsig.commons.utils.FileNameUtils;
+import es.icarto.gvsig.commons.utils.FileUtils;
+
 public class SHPExporter extends AbstractLayerExporter {
     private static Preferences prefs = Preferences.userRoot().node(
 	    "gvSIG.encoding.dbf");
@@ -73,89 +76,113 @@ public class SHPExporter extends AbstractLayerExporter {
 	}
     }
 
-    public void export(MapContext mapContext, FLyrVect layer, String path)
-	    throws BaseException {
-	if (!(path.toLowerCase().endsWith(".shp"))) {
-	    path = path + ".shp";
-	}
-	File newFile = new File(path);
-
+    private void exportMultiTypeGeom(MapContext mapContext, FLyrVect layer,
+	    String path) throws BaseException {
 	SelectableDataSource sds = layer.getRecordset();
 	FieldDescription[] fieldsDescrip = sds.getFieldsDescription();
+	ShpWriter writer1 = (ShpWriter) LayerFactory.getWM().getWriter(
+		"Shape Writer");
+	Driver[] drivers = new Driver[3];
+	ShpWriter[] writers = new ShpWriter[3];
+
+	// puntos
+	String auxPoint = path.replaceFirst("\\.shp", "_points.shp");
+
+	SHPLayerDefinition lyrDefPoint = new SHPLayerDefinition();
+	lyrDefPoint.setFieldsDesc(fieldsDescrip);
+	File filePoints = new File(auxPoint);
+	lyrDefPoint.setFile(filePoints);
+	lyrDefPoint.setName(filePoints.getName());
+	lyrDefPoint.setShapeType(FShape.POINT);
+	loadEncoding(layer, writer1);
+	writer1.setFile(filePoints);
+	writer1.initialize(lyrDefPoint);
+	writers[0] = writer1;
+	drivers[0] = getOpenShpDriver(filePoints);
+	// drivers[0]=null;
+
+	ShpWriter writer2 = (ShpWriter) LayerFactory.getWM().getWriter(
+		"Shape Writer");
+	// Lineas
+	String auxLine = path.replaceFirst("\\.shp", "_line.shp");
+	SHPLayerDefinition lyrDefLine = new SHPLayerDefinition();
+	lyrDefLine.setFieldsDesc(fieldsDescrip);
+
+	File fileLines = new File(auxLine);
+	lyrDefLine.setFile(fileLines);
+	lyrDefLine.setName(fileLines.getName());
+	lyrDefLine.setShapeType(FShape.LINE);
+	loadEncoding(layer, writer2);
+	writer2.setFile(fileLines);
+	writer2.initialize(lyrDefLine);
+	writers[1] = writer2;
+	drivers[1] = getOpenShpDriver(fileLines);
+	// drivers[1]=null;
+
+	ShpWriter writer3 = (ShpWriter) LayerFactory.getWM().getWriter(
+		"Shape Writer");
+	// Polï¿½gonos
+	String auxPolygon = path.replaceFirst("\\.shp", "_polygons.shp");
+	SHPLayerDefinition lyrDefPolygon = new SHPLayerDefinition();
+	lyrDefPolygon.setFieldsDesc(fieldsDescrip);
+	File filePolygons = new File(auxPolygon);
+	lyrDefPolygon.setFile(filePolygons);
+	lyrDefPolygon.setName(filePolygons.getName());
+	lyrDefPolygon.setShapeType(FShape.POLYGON);
+	loadEncoding(layer, writer3);
+	writer3.setFile(filePolygons);
+	writer3.initialize(lyrDefPolygon);
+	writers[2] = writer3;
+	drivers[2] = getOpenShpDriver(filePolygons);
+	// drivers[2]=null;
+
+	writeMultiFeatures(mapContext, layer, writers, drivers);
+    }
+
+    private void exportSingleTypeGeom(MapContext mapContext, FLyrVect layer,
+	    String path) throws BaseException {
+	path = FileNameUtils.ensureExtension(path, ".shp");
+	File newFile = new File(path);
+	ensureNeededFilesExists(newFile);
+	IndexedShpDriver reader = getOpenShpDriver(newFile);
+	exportSingleTypeGeom(mapContext, layer, path, reader);
+    }
+
+    /**
+     * Este método existe únicamente para instanciar un WriterTask que no
+     * pregunte para cada capa exportada si hay que añadirla al TOC
+     */
+    public void exportSingleTypeGeom(MapContext mapContext, FLyrVect layer,
+	    String path, Driver reader) throws BaseException {
+	path = FileNameUtils.ensureExtension(path, ".shp");
+	File newFile = new File(path);
+	ensureNeededFilesExists(newFile);
+	SelectableDataSource sds = layer.getRecordset();
+	FieldDescription[] fieldsDescrip = sds.getFieldsDescription();
+	ShpWriter writer = (ShpWriter) LayerFactory.getWM().getWriter(
+		"Shape Writer");
+	loadEncoding(layer, writer);
+	SHPLayerDefinition lyrDef = new SHPLayerDefinition();
+	lyrDef.setFieldsDesc(fieldsDescrip);
+	lyrDef.setFile(newFile);
+	lyrDef.setName(newFile.getName());
+	lyrDef.setShapeType(layer.getTypeIntVectorLayer());
+	writer.setFile(newFile);
+	writer.initialize(lyrDef);
+	writeFeatures(mapContext, layer, writer, reader);
+    }
+
+    public void export(MapContext mapContext, FLyrVect layer, String path)
+	    throws BaseException {
+	path = FileNameUtils.ensureExtension(path, ".shp");
 
 	if (layer.getShapeType() == FShape.MULTI) {
-	    ShpWriter writer1 = (ShpWriter) LayerFactory.getWM().getWriter(
-		    "Shape Writer");
-	    Driver[] drivers = new Driver[3];
-	    ShpWriter[] writers = new ShpWriter[3];
-
-	    // puntos
-	    String auxPoint = path.replaceFirst("\\.shp", "_points.shp");
-
-	    SHPLayerDefinition lyrDefPoint = new SHPLayerDefinition();
-	    lyrDefPoint.setFieldsDesc(fieldsDescrip);
-	    File filePoints = new File(auxPoint);
-	    lyrDefPoint.setFile(filePoints);
-	    lyrDefPoint.setName(filePoints.getName());
-	    lyrDefPoint.setShapeType(FShape.POINT);
-	    loadEncoding(layer, writer1);
-	    writer1.setFile(filePoints);
-	    writer1.initialize(lyrDefPoint);
-	    writers[0] = writer1;
-	    drivers[0] = getOpenShpDriver(filePoints);
-	    // drivers[0]=null;
-
-	    ShpWriter writer2 = (ShpWriter) LayerFactory.getWM().getWriter(
-		    "Shape Writer");
-	    // Lineas
-	    String auxLine = path.replaceFirst("\\.shp", "_line.shp");
-	    SHPLayerDefinition lyrDefLine = new SHPLayerDefinition();
-	    lyrDefLine.setFieldsDesc(fieldsDescrip);
-
-	    File fileLines = new File(auxLine);
-	    lyrDefLine.setFile(fileLines);
-	    lyrDefLine.setName(fileLines.getName());
-	    lyrDefLine.setShapeType(FShape.LINE);
-	    loadEncoding(layer, writer2);
-	    writer2.setFile(fileLines);
-	    writer2.initialize(lyrDefLine);
-	    writers[1] = writer2;
-	    drivers[1] = getOpenShpDriver(fileLines);
-	    // drivers[1]=null;
-
-	    ShpWriter writer3 = (ShpWriter) LayerFactory.getWM().getWriter(
-		    "Shape Writer");
-	    // Polï¿½gonos
-	    String auxPolygon = path.replaceFirst("\\.shp", "_polygons.shp");
-	    SHPLayerDefinition lyrDefPolygon = new SHPLayerDefinition();
-	    lyrDefPolygon.setFieldsDesc(fieldsDescrip);
-	    File filePolygons = new File(auxPolygon);
-	    lyrDefPolygon.setFile(filePolygons);
-	    lyrDefPolygon.setName(filePolygons.getName());
-	    lyrDefPolygon.setShapeType(FShape.POLYGON);
-	    loadEncoding(layer, writer3);
-	    writer3.setFile(filePolygons);
-	    writer3.initialize(lyrDefPolygon);
-	    writers[2] = writer3;
-	    drivers[2] = getOpenShpDriver(filePolygons);
-	    // drivers[2]=null;
-
-	    writeMultiFeatures(mapContext, layer, writers, drivers);
+	    exportMultiTypeGeom(mapContext, layer, path);
 	} else {
-	    ShpWriter writer = (ShpWriter) LayerFactory.getWM().getWriter(
-		    "Shape Writer");
-	    loadEncoding(layer, writer);
-	    IndexedShpDriver drv = getOpenShpDriver(newFile);
-	    SHPLayerDefinition lyrDef = new SHPLayerDefinition();
-	    lyrDef.setFieldsDesc(fieldsDescrip);
-	    lyrDef.setFile(newFile);
-	    lyrDef.setName(newFile.getName());
-	    lyrDef.setShapeType(layer.getTypeIntVectorLayer());
-	    writer.setFile(newFile);
-	    writer.initialize(lyrDef);
-	    writeFeatures(mapContext, layer, writer, drv);
+	    exportSingleTypeGeom(mapContext, layer, path);
 	}
     }
+
 
     private void loadEncoding(FLyrVect layer, ShpWriter writer) {
 	String charSetName = prefs.get("dbf_encoding", DbaseFile
@@ -171,16 +198,10 @@ public class SHPExporter extends AbstractLayerExporter {
 	}
     }
 
-    /**
-     * Loads the dbf enconding
-     * 
-     * @param layer
-     * @param writer
-     */
-    private IndexedShpDriver getOpenShpDriver(File fileShp)
-	    throws OpenDriverException {
-	IndexedShpDriver drv = new IndexedShpDriver();
+    private void ensureNeededFilesExists(File fileShp)
+	    throws FileNotFoundDriverException {
 	if (!fileShp.exists()) {
+	    fileShp.getParentFile().mkdirs();
 	    try {
 		fileShp.createNewFile();
 		File newFileSHX = new File(fileShp.getAbsolutePath()
@@ -194,6 +215,17 @@ public class SHPExporter extends AbstractLayerExporter {
 			fileShp.getAbsolutePath());
 	    }
 	}
+    }
+
+    /**
+     * Loads the dbf enconding
+     *
+     * @param layer
+     * @param writer
+     */
+    private IndexedShpDriver getOpenShpDriver(File fileShp)
+	    throws OpenDriverException {
+	IndexedShpDriver drv = new IndexedShpDriver();
 	drv.open(fileShp);
 	return drv;
     }
