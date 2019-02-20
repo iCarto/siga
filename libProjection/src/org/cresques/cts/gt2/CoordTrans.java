3d54c227213ee9b91176a8a968a70144e36f7fe2
Jorge López Fernández <lopez.fernandez.jorge@gmail.com>
2019-02-20 18:39:17 +0100

    Fix reprojecting related code
    It's used in multiple places and the old version
    raised problems when changing between datums.
    
    refs #1428

--- libProjection/src/org/cresques/cts/gt2/CoordTrans.java
+++ libProjection/src/org/cresques/cts/gt2/CoordTrans.java
@@ -28,11 +28,13 @@ import java.awt.geom.Rectangle2D;
 
 import org.cresques.cts.ICoordTrans;
 import org.cresques.cts.IProjection;
-import org.geotools.ct.CannotCreateTransformException;
-import org.geotools.ct.CoordinateTransformation;
-import org.geotools.ct.CoordinateTransformationFactory;
-import org.geotools.ct.MathTransform;
 import org.geotools.pt.CoordinatePoint;
+import org.geotools.referencing.CRS;
+import org.geotools.referencing.operation.DefaultCoordinateOperationFactory;
+import org.opengis.referencing.FactoryException;
+import org.opengis.referencing.NoSuchAuthorityCodeException;
+import org.opengis.referencing.operation.CoordinateOperation;
+import org.opengis.referencing.operation.OperationNotFoundException;
 import org.opengis.referencing.operation.TransformException;
 
 
@@ -44,70 +46,28 @@ import org.opengis.referencing.operation.TransformException;
  * @author "Luis W. Sevilla" <sevilla_lui@gva.es>
  */
 class CoordTrans implements ICoordTrans {
-    private CoordinateTransformationFactory trFactory = CoordinateTransformationFactory.getDefault();
-    private CoordinateTransformation tr = null;
-    private MathTransform mt = null;
-    private MathTransform mt2 = null;
-    private MathTransform mt3 = null;
-    private MathTransform mtDatum = null;
+    private DefaultCoordinateOperationFactory trFactory = new DefaultCoordinateOperationFactory();
+    private CoordinateOperation op = null;
     private CoordSys from = null;
     private CoordSys to = null;
-    
+
     private ICoordTrans invertedCT = null;
 
     public CoordTrans(CoordSys from, CoordSys to) {
         this.from = from;
         this.to = to;
 
-        // Si los dos CoordSys son proyectados, entonces hay
-        // que hacer dos transformaciones (pasar por geográficas)
-        // Si hay cambio de datum son 3 las transformaciones
         try {
-            if (from.getDatum() != to.getDatum()) {
-                tr = trFactory.createFromCoordinateSystems(from.toGeo().getCS(),
-                                                           to.toGeo().getCS());
-                mtDatum = tr.getMathTransform();
-            }
-
-            if ((from.projCS != null) && (to.projCS != null)) {
-                CoordSys geogcs = from.toGeo();
-                tr = trFactory.createFromCoordinateSystems(from.getCS(),
-                                                           geogcs.getCS());
-                mt = tr.getMathTransform();
-
-                if (mtDatum != null) {
-                    mt2 = mtDatum;
-                }
-
-                geogcs = to.toGeo();
-                tr = trFactory.createFromCoordinateSystems(geogcs.getCS(),
-                                                           to.getCS());
-
-                if (mt2 == null) {
-                    mt2 = tr.getMathTransform();
-                } else {
-                    mt3 = tr.getMathTransform();
-                }
-            } else {
-                if (from.projCS == null) {
-                    mt = mtDatum;
-                }
-
-                tr = trFactory.createFromCoordinateSystems(from.getCS(),
-                                                           to.getCS());
-
-                if (mt == null) {
-                    mt = tr.getMathTransform();
-
-                    if (mtDatum != null) {
-                        mt2 = mtDatum;
-                    }
-                } else {
-                    mt2 = tr.getMathTransform();
-                }
-            }
-        } catch (CannotCreateTransformException e) {
-            // TODO Bloque catch generado automáticamente
+            op = trFactory.createOperation(CRS.decode(from.getAbrev()),
+                    CRS.decode(to.getAbrev()));
+        } catch (OperationNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (NoSuchAuthorityCodeException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (FactoryException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
@@ -132,23 +92,8 @@ class CoordTrans implements ICoordTrans {
         ptDest = null;
 
         try {
-            mt.transform(pt1, pt2);
+            op.getMathTransform().transform(pt1, pt2);
             ptDest = pt2.toPoint2D();
-
-            if (mt2 != null) {
-                mt2.transform(pt2, pt1);
-                ptDest = pt1.toPoint2D();
-
-                if (mt3 != null) {
-                    mt3.transform(pt1, pt2);
-                    ptDest = pt2.toPoint2D();
-                }
-            }
-
-            /*} catch (MismatchedDimensionException e) {
-                    // TODO Bloque catch generado automáticamente
-                    e.printStackTrace();
-            */
         } catch (TransformException e) {
             // TODO Bloque catch generado automáticamente
             e.printStackTrace();
@@ -158,7 +103,7 @@ class CoordTrans implements ICoordTrans {
     }
 
     public String toString() {
-        return tr.toString();
+        return op.toString();
     }
 
     /* (non-Javadoc)
