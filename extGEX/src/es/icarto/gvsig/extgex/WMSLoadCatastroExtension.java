77992665d53a25730689c4a9b3635e1374245e8d
Francisco Puga <fpuga@icarto.es>
2019-03-06 10:39:38 +0100

    extGEX: Small refactor for WMS classes
    * Now inherit from AbstractExtension
    * Renames dimensiones wms class and add an icon
    
    refs #1624

--- extGEX/src/es/icarto/gvsig/extgex/WMSLoadCatastroExtension.java
+++ extGEX/src/es/icarto/gvsig/extgex/WMSLoadCatastroExtension.java
@@ -1,47 +1,25 @@
 package es.icarto.gvsig.extgex;
 
-import com.iver.andami.PluginServices;
-import com.iver.andami.plugins.Extension;
-import com.iver.andami.ui.mdiManager.IWindow;
-import com.iver.cit.gvsig.project.documents.view.gui.View;
-
+import es.icarto.gvsig.commons.AbstractExtension;
 import es.icarto.gvsig.extgex.wms.LoadWMS;
 import es.udc.cartolab.gvsig.users.utils.DBSession;
 
-public class WMSLoadCatastroExtension extends Extension {
+public class WMSLoadCatastroExtension extends AbstractExtension {
 
     @Override
     public void execute(String actionCommand) {
-	LoadWMS loadWMS = new LoadWMS("Catastro_WMS");
-	loadWMS.Load();
-    }
-
-    @Override
-    public void initialize() {
-	// Nothing to do
+        LoadWMS loadWMS = new LoadWMS("Catastro_WMS");
+        loadWMS.Load();
     }
 
     @Override
     public boolean isEnabled() {
-	if ((DBSession.getCurrentSession() != null) &&
-		(hasView())) {
-	    return true;
-	} else {
-	    return false;
-	}
+        return DBSession.isActive() && isViewActive();
     }
 
     @Override
-    public boolean isVisible() {
-	return true;
-    }
-
-    private boolean hasView() {
-	IWindow window = PluginServices.getMDIManager().getActiveWindow();
-	if(window instanceof View) {
-	    return true;
-	}
-	return false;
+    public void initialize() {
+        // Overrided, because there is no toolbar icon for this extension
     }
 
 }
