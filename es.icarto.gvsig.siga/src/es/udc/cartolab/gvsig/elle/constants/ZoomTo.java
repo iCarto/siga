4892b0337fc6d29588dbad524052d924650afc52
Francisco Puga <fpuga@icarto.es>
2019-03-29 21:40:34 +0100

    siga. Refactor on ZoomTo to extract how the point is drawn in the graphic layer
    refs #1418

--- es.icarto.gvsig.siga/src/es/udc/cartolab/gvsig/elle/constants/ZoomTo.java
+++ es.icarto.gvsig.siga/src/es/udc/cartolab/gvsig/elle/constants/ZoomTo.java
@@ -1,18 +1,11 @@
 package es.udc.cartolab.gvsig.elle.constants;
 
-import java.awt.Color;
 import java.awt.geom.Rectangle2D;
 
 import com.iver.cit.gvsig.fmap.MapControl;
 import com.iver.cit.gvsig.fmap.ViewPort;
-import com.iver.cit.gvsig.fmap.core.FShape;
-import com.iver.cit.gvsig.fmap.core.IGeometry;
-import com.iver.cit.gvsig.fmap.core.ShapeFactory;
-import com.iver.cit.gvsig.fmap.core.SymbologyFactory;
-import com.iver.cit.gvsig.fmap.core.symbols.ISymbol;
-import com.iver.cit.gvsig.fmap.layers.GraphicLayer;
-import com.iver.cit.gvsig.fmap.rendering.FGraphic;
 
+import es.icarto.gvsig.commons.map.OverlayPoint;
 import es.icarto.gvsig.commons.referencing.GPoint;
 import es.icarto.gvsig.commons.referencing.PostgisTransformation;
 
@@ -20,74 +13,63 @@ public class ZoomTo {
 
     private final MapControl mapControl;
     private PostgisTransformation transform;
+    private final OverlayPoint overlayPoint;
 
     public ZoomTo(MapControl mapControl) {
-	this.mapControl = mapControl;
+        this.mapControl = mapControl;
+        this.overlayPoint = new OverlayPoint(mapControl);
     }
 
     public void setTranformation(PostgisTransformation transform) {
-	this.transform = transform;
+        this.transform = transform;
     }
 
     public void zoom(GPoint shape, boolean drawPoint) {
-	if (shape == null) {
-	    return;
-	}
-	GPoint reShape = transform.transform(shape, mapControl.getProjection());
-	Rectangle2D bbox = getGeometry(reShape.getBounds2D());
-	if (bbox != null) {
-	    if (drawPoint) {
-		drawPoint(reShape.getX(), reShape.getY());
-	    }
-	    zoomTo(bbox);
-	}
-    }
+        if (shape == null) {
+            return;
+        }
 
-    private void drawPoint(double x, double y) {
-	GraphicLayer lyr = mapControl.getMapContext().getGraphicsLayer();
-	lyr.clearAllGraphics();
-	Color color = Color.red;
-	ISymbol theSymbol = SymbologyFactory.createDefaultSymbolByShapeType(
-		FShape.POINT, color);
-
-	int idSymbol = lyr.addSymbol(theSymbol);
-	IGeometry geom = ShapeFactory.createPoint2D(x, y);
-	FGraphic theGraphic = new FGraphic(geom, idSymbol);
-	lyr.addGraphic(theGraphic);
-	// mapControl.drawGraphics();
-
+        GPoint reShape = new GPoint(shape.getProjection(), shape.getX(), shape.getY());
+        if (transform != null) {
+            reShape = transform.transform(shape, mapControl.getProjection());
+        }
+        Rectangle2D bbox = getGeometry(reShape.getBounds2D());
+        if (bbox != null) {
+            if (drawPoint) {
+                overlayPoint.draw(reShape.getX(), reShape.getY());
+            }
+            zoomTo(bbox);
+        }
     }
 
     public void zoom(Rectangle2D rectangle) {
-	if (rectangle == null) {
-	    return;
-	}
-	Rectangle2D bbox = getGeometry(rectangle);
-	if (bbox != null) {
-	    zoomTo(bbox);
-	}
+        if (rectangle == null) {
+            return;
+        }
+        Rectangle2D bbox = getGeometry(rectangle);
+        if (bbox != null) {
+            zoomTo(bbox);
+        }
     }
 
     private Rectangle2D getGeometry(Rectangle2D rectangle) {
-	if (rectangle.getWidth() < 200) {
-	    rectangle.setFrameFromCenter(rectangle.getCenterX(),
-		    rectangle.getCenterY(), rectangle.getCenterX() + 100,
-		    rectangle.getCenterY() + 100);
-	}
-	return rectangle;
+        if (rectangle.getWidth() < 200) {
+            rectangle.setFrameFromCenter(rectangle.getCenterX(), rectangle.getCenterY(), rectangle.getCenterX() + 100,
+                    rectangle.getCenterY() + 100);
+        }
+        return rectangle;
     }
 
     private Rectangle2D zoomTo(Rectangle2D rectangle) {
-	if (rectangle != null) {
-	    final ViewPort viewPort = mapControl.getMapContext().getViewPort();
-	    viewPort.setExtent(rectangle);
-	    viewPort.refreshExtent();
-	}
-	return rectangle;
+        if (rectangle != null) {
+            final ViewPort viewPort = mapControl.getMapContext().getViewPort();
+            viewPort.setExtent(rectangle);
+            viewPort.refreshExtent();
+        }
+        return rectangle;
     }
 
-    public void resetPoint() {
-	GraphicLayer lyr = mapControl.getMapContext().getGraphicsLayer();
-	lyr.clearAllGraphics();
+    public void clearAllGraphics() {
+        this.overlayPoint.clearAllGraphics();
     }
 }
