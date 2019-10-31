edeae9fec6a1f89db535f2b9437ed2141f369b09
Francisco Puga <fpuga@icarto.es>
2019-10-31 13:10:03 +0100

    Two field on enlaces are automatically calculated
    "numero_ramales" and "longitud_ramales" are automatically calculated
    with a db trigger on the tables "ramales" and "enlaces". To "notify"
    the "Enlaces" layer and the "Enlaces" form about this change custom
    code is writen in `RamalesForm.saveRecord`.
    
    Still, if a "ramal" is removed, this logic, fails. Probably
    `AbstractNavtable.deleteRecord` should be also overriden
    
    refs #1779

--- extGIA/src/es/icarto/gvsig/extgia/forms/enlaces/EnlacesForm.java
+++ extGIA/src/es/icarto/gvsig/extgia/forms/enlaces/EnlacesForm.java
@@ -8,8 +8,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.EventListener;
-import java.util.List;
 import java.util.Map;
 
 import javax.swing.JComponent;
@@ -18,12 +16,9 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
-import javax.swing.event.TableModelEvent;
-import javax.swing.event.TableModelListener;
 
 import org.apache.log4j.Logger;
 
-import com.hardcode.gdbms.driver.exceptions.ReadDriverException;
 import com.iver.cit.gvsig.fmap.layers.FLyrVect;
 
 import es.icarto.gvsig.extgia.forms.AbstractFormWithLocationWidgets;
@@ -32,61 +27,55 @@ import es.icarto.gvsig.extgia.forms.GIAAlphanumericTableHandler;
 import es.icarto.gvsig.extgia.forms.ramales.RamalesForm;
 import es.icarto.gvsig.extgia.preferences.DBFieldNames;
 import es.icarto.gvsig.extgia.preferences.Elements;
-import es.icarto.gvsig.navtableforms.AbstractForm;
-import es.icarto.gvsig.navtableforms.gui.tables.handler.BaseTableHandler;
 import es.icarto.gvsig.navtableforms.gui.tables.handler.VectorialTableHandler;
 import es.udc.cartolab.gvsig.users.utils.DBSession;
 
 @SuppressWarnings("serial")
 public class EnlacesForm extends AbstractFormWithLocationWidgets {
-	
-	private static final Logger logger = Logger.getLogger(AbstractForm.class);
+
+    private static final Logger logger = Logger.getLogger(EnlacesForm.class);
 
     public static final String TABLENAME = "enlaces";
-    private TableModelListener ramalesTableModelListener;
-    private VectorialTableHandler ramalesTableHandler;
+    public static final String TOCLAYERNAME = "Enlaces";
+
+    private final VectorialTableHandler ramalesTableHandler;
 
     JTextField enlaceIDWidget;
     CalculateComponentValue enlaceid;
 
-    public static String[] carreterasColNames = { "id_carretera_enlazada",
-	"clave_carretera", "pk", "titular", "tipo_cruce" };
+    public static String[] carreterasColNames = { "id_carretera_enlazada", "clave_carretera", "pk", "titular",
+    "tipo_cruce" };
 
-    public static String[] carreterasColAlias = { "ID Carretera", "Clave",
-	"PK", "Titular", "Tipo Cruce" };
+    public static String[] carreterasColAlias = { "ID Carretera", "Clave", "PK", "Titular", "Tipo Cruce" };
 
     public EnlacesForm(FLyrVect layer) {
-	super(layer);
+        super(layer);
 
-	addTableHandler(new GIAAlphanumericTableHandler(
-		getReconocimientosDBTableName(), getWidgets(), getElementID(),
-		DBFieldNames.reconocimientosWhitoutIndexColNames,
-		DBFieldNames.reconocimientosWhitoutIndexColAlias, null, this));
+        addTableHandler(new GIAAlphanumericTableHandler(getReconocimientosDBTableName(), getWidgets(), getElementID(),
+                DBFieldNames.reconocimientosWhitoutIndexColNames, DBFieldNames.reconocimientosWhitoutIndexColAlias,
+                null, this));
 
-	addTableHandler(new GIAAlphanumericTableHandler(
-		"enlaces_carreteras_enlazadas", getWidgets(), getElementID(),
-		carreterasColNames, carreterasColAlias, null, this));
+        addTableHandler(new GIAAlphanumericTableHandler("enlaces_carreteras_enlazadas", getWidgets(), getElementID(),
+                carreterasColNames, carreterasColAlias, null, this));
 
-	ramalesTableHandler = new VectorialTableHandler(RamalesForm.TABLENAME,
-			getWidgets(), new String[] { TRAMO, TIPO_VIA, NOMBRE_VIA },
-			RamalesForm.colNames, RamalesForm.colAlias);
-	addTableHandler(ramalesTableHandler);
+        ramalesTableHandler = new VectorialTableHandler(RamalesForm.TABLENAME, getWidgets(), new String[] { TRAMO,
+            TIPO_VIA, NOMBRE_VIA }, RamalesForm.colNames, RamalesForm.colAlias);
+        addTableHandler(ramalesTableHandler);
     }
 
     @Override
     protected void setListeners() {
-	super.setListeners();
-	Map<String, JComponent> widgets = getWidgets();
+        super.setListeners();
+        Map<String, JComponent> widgets = getWidgets();
 
-	enlaceIDWidget = (JTextField) widgets.get(DBFieldNames.ID_ENLACE);
+        enlaceIDWidget = (JTextField) widgets.get(DBFieldNames.ID_ENLACE);
 
-	enlaceid = new EnlacesCalculateIDValue(this, getWidgetComponents(),
-		DBFieldNames.ID_ENLACE, DBFieldNames.AREA_MANTENIMIENTO,
-		DBFieldNames.BASE_CONTRATISTA, DBFieldNames.TRAMO,
-		DBFieldNames.TIPO_VIA, DBFieldNames.MUNICIPIO, DBFieldNames.PK);
-	enlaceid.setListeners();
+        enlaceid = new EnlacesCalculateIDValue(this, getWidgetComponents(), DBFieldNames.ID_ENLACE,
+                DBFieldNames.AREA_MANTENIMIENTO, DBFieldNames.BASE_CONTRATISTA, DBFieldNames.TRAMO,
+                DBFieldNames.TIPO_VIA, DBFieldNames.MUNICIPIO, DBFieldNames.PK);
+        enlaceid.setListeners();
 
-	setIdWhenPkIsAutomaticallyUpdated();
+        setIdWhenPkIsAutomaticallyUpdated();
 
     }
 
@@ -97,126 +86,89 @@ public class EnlacesForm extends AbstractFormWithLocationWidgets {
      */
     private void setIdWhenPkIsAutomaticallyUpdated() {
 
-	JTextField textField = (JTextField) getFormBody().getComponentByName(
-		DBFieldNames.PK);
-	textField.getDocument().addDocumentListener(new DocumentListener() {
-	    @Override
-	    public void changedUpdate(DocumentEvent e) {
-		doIt();
-	    }
+        JTextField textField = (JTextField) getFormBody().getComponentByName(DBFieldNames.PK);
+        textField.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                doIt();
+            }
 
-	    @Override
-	    public void removeUpdate(DocumentEvent e) {
-		doIt();
-	    }
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                doIt();
+            }
 
-	    @Override
-	    public void insertUpdate(DocumentEvent e) {
-		doIt();
-	    }
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                doIt();
+            }
 
-	    private void doIt() {
-		if (!isFillingValues()) {
-		    enlaceid.setValue(true);
-		}
-	    }
-	});
+            private void doIt() {
+                if (!isFillingValues()) {
+                    enlaceid.setValue(true);
+                }
+            }
+        });
     }
 
     @Override
     protected boolean validationHasErrors() {
-	if (this.getFormController().getValuesChanged()
-		.containsKey("id_enlace")) {
-	    if (enlaceIDWidget.getText() != "") {
-		String query = "SELECT id_enlace FROM audasa_extgia.enlaces "
-			+ " WHERE id_enlace = '" + enlaceIDWidget.getText()
-			+ "';";
-		PreparedStatement statement = null;
-		Connection connection = DBSession.getCurrentSession()
-			.getJavaConnection();
-		try {
-		    statement = connection.prepareStatement(query);
-		    statement.execute();
-		    ResultSet rs = statement.getResultSet();
-		    if (rs.next()) {
-			JOptionPane.showMessageDialog(null,
-				"El ID está en uso, por favor, escoja otro.",
-				"ID en uso", JOptionPane.WARNING_MESSAGE);
-			return true;
-		    }
-		} catch (SQLException e) {
-		    e.printStackTrace();
-		}
-	    }
-	}
-	return super.validationHasErrors();
+        if (this.getFormController().getValuesChanged().containsKey("id_enlace")) {
+            if (enlaceIDWidget.getText() != "") {
+                String query = "SELECT id_enlace FROM audasa_extgia.enlaces " + " WHERE id_enlace = '"
+                        + enlaceIDWidget.getText() + "';";
+                PreparedStatement statement = null;
+                Connection connection = DBSession.getCurrentSession().getJavaConnection();
+                try {
+                    statement = connection.prepareStatement(query);
+                    statement.execute();
+                    ResultSet rs = statement.getResultSet();
+                    if (rs.next()) {
+                        JOptionPane.showMessageDialog(null, "El ID está en uso, por favor, escoja otro.", "ID en uso",
+                                JOptionPane.WARNING_MESSAGE);
+                        return true;
+                    }
+                } catch (SQLException e) {
+                    logger.error(e.getStackTrace(), e);
+                }
+            }
+        }
+        return super.validationHasErrors();
     }
 
     @Override
-    protected void fillSpecificValues() {
-    	updateAutomaticFieldLongitudRamalesWhenSubFormDataChanges_pre();
-        super.fillSpecificValues();
-        updateAutomaticFieldLongitudRamalesWhenSubFormDataChanges_post();
-    }
-    
-    @Override
     public JTable getReconocimientosJTable() {
-	// TODO Auto-generated method stub
-	return null;
+        return null;
     }
 
     @Override
     public JTable getTrabajosJTable() {
-	// TODO Auto-generated method stub
-	return null;
+        return null;
     }
 
     @Override
     public String getBasicName() {
-	return TABLENAME;
+        return TABLENAME;
     }
 
     @Override
     public Elements getElement() {
-	return Elements.Enlaces;
+        return Elements.Enlaces;
     }
 
     @Override
     protected boolean hasSentido() {
-	// TODO Auto-generated method stub
-	return false;
+        return false;
     }
 
     @Override
     public String getElementID() {
-	return "id_enlace";
+        return "id_enlace";
     }
 
     @Override
     public String getElementIDValue() {
-	return enlaceIDWidget.getText();
-    }
-    
-    private void updateAutomaticFieldLongitudRamalesWhenSubFormDataChanges_pre() {
-        if (ramalesTableModelListener != null) {
-          ramalesTableHandler.getModel().removeTableModelListener(ramalesTableModelListener);   
-        }
-    }
-
-    private void updateAutomaticFieldLongitudRamalesWhenSubFormDataChanges_post() {
-        ramalesTableModelListener = new TableModelListener() {
-
-            @Override
-            public void tableChanged(TableModelEvent e) {
-                try {
-                    EnlacesForm.this.reloadRecordset();
-                } catch (ReadDriverException ex) {
-                    logger.error(ex.getStackTrace(), ex);
-                }
-                EnlacesForm.this.onPositionChange(null);
-            }
-        };
-        ramalesTableHandler.getModel().addTableModelListener(ramalesTableModelListener); 
+        return enlaceIDWidget.getText();
     }
 
 }
--- extGIA/src/es/icarto/gvsig/extgia/forms/ramales/RamalesForm.java
+++ extGIA/src/es/icarto/gvsig/extgia/forms/ramales/RamalesForm.java
@@ -3,59 +3,96 @@ package es.icarto.gvsig.extgia.forms.ramales;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 
+import org.apache.log4j.Logger;
+
+import com.iver.andami.PluginServices;
+import com.iver.andami.ui.mdiManager.IWindow;
+import com.iver.cit.gvsig.exceptions.layers.ReloadLayerException;
+import com.iver.cit.gvsig.exceptions.visitors.StopWriterVisitorException;
 import com.iver.cit.gvsig.fmap.layers.FLyrVect;
 
 import es.icarto.gvsig.extgia.forms.AbstractFormWithLocationWidgets;
+import es.icarto.gvsig.extgia.forms.enlaces.EnlacesForm;
 import es.icarto.gvsig.extgia.preferences.Elements;
+import es.icarto.gvsig.navtableforms.utils.TOCLayerManager;
 
 @SuppressWarnings("serial")
 public class RamalesForm extends AbstractFormWithLocationWidgets {
 
-    public static final String[] colNames = { "gid", "ramal", "sentido",
-	    "direccion", "longitud" };
-    public static final String[] colAlias = { "ID Ramal", "Nombre Ramal",
-	    "Sentido", "Dirección", "Longitud" };
+    private static final Logger logger = Logger.getLogger(RamalesForm.class);
+
+    public static final String[] colNames = { "gid", "ramal", "sentido", "direccion", "longitud" };
+    public static final String[] colAlias = { "ID Ramal", "Nombre Ramal", "Sentido", "Dirección", "Longitud" };
 
     public RamalesForm(FLyrVect layer) {
-	super(layer);
+        super(layer);
     }
 
     public static final String TABLENAME = "ramales";
 
     @Override
     public Elements getElement() {
-	return Elements.Ramales;
+        return Elements.Ramales;
     }
 
     @Override
     public String getElementID() {
-	return "gid";
+        return "gid";
     }
 
     @Override
     public String getElementIDValue() {
-	JTextField idWidget = (JTextField) getWidgets().get(getElementID());
-	return idWidget.getText();
+        JTextField idWidget = (JTextField) getWidgets().get(getElementID());
+        return idWidget.getText();
     }
 
     @Override
     public JTable getReconocimientosJTable() {
-	return null;
+        return null;
     }
 
     @Override
     public JTable getTrabajosJTable() {
-	return null;
+        return null;
     }
 
     @Override
     public String getBasicName() {
-	return TABLENAME;
+        return TABLENAME;
     }
 
     @Override
     protected boolean hasSentido() {
-	return true;
+        return true;
     }
 
+    @Override
+    public boolean saveRecord() throws StopWriterVisitorException {
+        boolean flag = super.saveRecord();
+        if (!flag) {
+            // Ha habido algún problema en el guardado
+            return flag;
+        }
+
+        FLyrVect enlacesLyr = new TOCLayerManager().getLayerByName(EnlacesForm.TOCLAYERNAME);
+        if (enlacesLyr == null) {
+            return flag;
+        }
+
+        try {
+            enlacesLyr.reload();
+        } catch (ReloadLayerException e) {
+            logger.error(e.getStackTrace(), e);
+            return false;
+        }
+
+        IWindow[] allWindows = PluginServices.getMDIManager().getAllWindows();
+        for (IWindow iWindow : allWindows) {
+            if (iWindow instanceof EnlacesForm) {
+                EnlacesForm enlacesForm = (EnlacesForm) iWindow;
+                enlacesForm.onPositionChange(null);
+            }
+        }
+        return true;
+    }
 }
