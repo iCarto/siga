e98b9927096165be0fc852b5764a12822934caac
Francisco Puga <fpuga@icarto.es>
2020-02-21 18:32:44 +0100

    Minor changes on ExtractVertexesExtension
    * Removes boilerplate delegating to AbstractExtension utilities
    * Removes the view == null guard on execute. It can no be called
      by the user in this case because the button will be disabled. And
      if is called programatically it should callee responsability
    
    refs #1770

--- es.icarto.gvsig.siga/src/es/icarto/gvsig/siga/extractvertexestool/ExtractVertexesExtension.java
+++ es.icarto.gvsig.siga/src/es/icarto/gvsig/siga/extractvertexestool/ExtractVertexesExtension.java
@@ -1,7 +1,6 @@
 package es.icarto.gvsig.siga.extractvertexestool;
 
 import com.iver.andami.PluginServices;
-import com.iver.andami.ui.mdiManager.IWindow;
 import com.iver.cit.gvsig.fmap.layers.FLayers;
 import com.iver.cit.gvsig.project.documents.view.gui.View;
 
@@ -13,38 +12,29 @@ public class ExtractVertexesExtension extends AbstractExtension {
 
     @Override
     public void execute(String actionCommand) {
-        IWindow window = PluginServices.getMDIManager().getActiveWindow();
-        if (window instanceof View) {
-            updateGeoprocessPanel();
-            ExtractVertexesPaneContainer container = new ExtractVertexesPaneContainer(
-                    panel);
-            ExtractVertexesGeoprocess controller = new ExtractVertexesGeoprocess();
-            controller.setView(panel);
-            container.setCommand(controller);
-            container.validate();
-            container.repaint();
-            PluginServices.getMDIManager().addWindow(container);
-        }
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return PluginServices.getMDIManager().getActiveWindow() instanceof View;
-    }
-
-    @Override
-    public boolean isVisible() {
-        return true;
+        updateGeoprocessPanel();
+        ExtractVertexesPaneContainer container = new ExtractVertexesPaneContainer(panel);
+        ExtractVertexesGeoprocess controller = new ExtractVertexesGeoprocess();
+        controller.setView(panel);
+        container.setCommand(controller);
+        container.validate();
+        container.repaint();
+        PluginServices.getMDIManager().addWindow(container);
     }
 
     private void updateGeoprocessPanel() {
-        FLayers layers = ((View) PluginServices.getMDIManager()
-                .getActiveWindow()).getModel().getMapContext().getLayers();
+        FLayers layers = ((View) PluginServices.getMDIManager().getActiveWindow()).getModel().getMapContext()
+                .getLayers();
         if (panel == null) {
             panel = new ExtractVertexesGeoprocessPanel(layers);
         } else {
             panel.updateLayers(layers);
         }
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isViewActive();
     }
 
 }
