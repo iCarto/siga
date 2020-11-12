iCarto/siga

commit ace155ffda4bd969a1fb6dada753a0fe705cbc91

Fixes 'Consultas' issues after splitting 'Valla_Cierre'
* Excel libraries have a row limit, and trying to write many rows to a pdf
  can provoke in memory problems
* Data model changes in 'Valla_Cierre' where not propagated to 'Consultas'
* Changes how 'ignoredColumns' to not present some columns to the user in the
  custom report. Now it uses the same columns to ignore for 'Trabajos' and
  'Caracteristicas'

refs #2154

extGIA/src/es/icarto/gvsig/extgia/consultas/Leaf.java
@@ -10,6 +10,8 @@ import es.icarto.gvsig.commons.db.ConnectionWrapper;
 import es.icarto.gvsig.commons.gui.SaveFileDialog;
 import es.icarto.gvsig.commons.queries.Component;
 import es.icarto.gvsig.commons.queries.FinalActions;
+import es.icarto.gvsig.commons.queries.ReportValidation;
+import es.icarto.gvsig.commons.queries.ReportValidationResult;
 import es.icarto.gvsig.commons.queries.XLSReport;
 import es.icarto.gvsig.commons.utils.Field;
 import es.icarto.gvsig.extgia.consultas.agregados.TrabajosAgregadosReport;
@@ -20,13 +22,18 @@ import es.udc.cartolab.gvsig.users.utils.DBSession;
 
 public class Leaf implements Component {
 
+    
+    
     private final String[] element;
     private final ConsultasFilters<Field> consultasFilters;
     private final KeyValue tipoConsulta;
     private final boolean pdf;
 
     private File outputFile;
-    private boolean emptyQuery = false;
+
+
+    private final ReportValidation reportValidation;
+    private ReportValidationResult reportValidationResult;
 
     public Leaf(String[] element, ConsultasFilters<Field> consultasFilters,
 	    KeyValue tipoConsulta, boolean pdf) {
@@ -35,6 +42,7 @@ public class Leaf implements Component {
 	this.outputFile = new File("./" + element[0]);
 	this.tipoConsulta = tipoConsulta;
 	this.pdf = pdf;
+	this.reportValidation = new ReportValidation();
     }
 
     @Override
@@ -82,10 +90,12 @@ public class Leaf implements Component {
 	    ConnectionWrapper con = new ConnectionWrapper(DBSession
 		    .getCurrentSession().getJavaConnection());
 	    DefaultTableModel table = con.execute(query);
-	    if (table.getRowCount() == 0) {
-		emptyQuery = true;
-		return;
+	    
+	    reportValidationResult = reportValidation.afterGetResults(table);
+	    if (reportValidationResult.isError()) {
+	        return;
 	    }
+	    
 
 	    if (pdf) {
 		createPdfReport(tipo, outputFile.getAbsolutePath(), element,
@@ -241,7 +251,7 @@ public class Leaf implements Component {
 
     @Override
     public void finalActions() {
-	FinalActions finalActions = new FinalActions(emptyQuery, outputFile);
+	FinalActions finalActions = new FinalActions(outputFile, reportValidationResult);
 	finalActions.openReport();
     }
 

es.icarto.gvsig.siga/config/text_es.properties
@@ -39,6 +39,7 @@ es.icarto.gvsig.siga.LocatorByCoordsExtension.menu=Coordenadas
 
 # queries
 queryWithoutResults_msg=La consulta produjo 0 resultados
+queryWithTooMuchResults_msg=La consulta tiene demasiados resultados. Debe realizar un filtro más preciso para poder generar el reporte.
 reportSuccessfullyGenerated_msg=Listado generado con éxito en: \n
 openReport=Abrir listado
 openFolder=Abrir directorio

es.icarto.gvsig.siga/src/es/icarto/gvsig/commons/queries/FinalActions.java
@@ -14,18 +14,18 @@ public class FinalActions {
 
     private static final Logger logger = Logger.getLogger(FinalActions.class);
 
-    private final boolean emptyQuery;
     private final File output;
+    private final ReportValidationResult reportValidationResult;
 
-    public FinalActions(boolean emptyQuery, File output) {
-	this.emptyQuery = emptyQuery;
-	this.output = output;
+
+    public FinalActions(File outputFile, ReportValidationResult reportValidationResult) {
+        this.output = outputFile;
+        this.reportValidationResult = reportValidationResult;
     }
 
     public void showMessages() {
-	if (emptyQuery) {
-	    JOptionPane.showMessageDialog(null,
-		    PluginServices.getText(this, "queryWithoutResults_msg"));
+	if (reportValidationResult.isError()) {
+	    JOptionPane.showMessageDialog(null, reportValidationResult.getMsg());
 	} else {
 	    JOptionPane.showMessageDialog(
 		    null,
@@ -36,9 +36,8 @@ public class FinalActions {
     }
 
     public void openReport() {
-	if (emptyQuery) {
-	    JOptionPane.showMessageDialog(null,
-		    PluginServices.getText(this, "queryWithoutResults_msg"));
+        if (reportValidationResult.isError()) {
+            JOptionPane.showMessageDialog(null, reportValidationResult.getMsg());
 	} else {
 	    showOpenReportDialog("openReport");
 	}
@@ -65,13 +64,11 @@ public class FinalActions {
     }
 
     public void openFolder() {
-	if (emptyQuery) {
-	    JOptionPane.showMessageDialog(null,
-		    PluginServices.getText(this, "queryWithoutResults_msg"));
-	} else {
-	    showOpenReportDialog("openFolder");
-	}
-
+        if (reportValidationResult.isError()) {
+            JOptionPane.showMessageDialog(null, reportValidationResult.getMsg());
+        } else {
+            showOpenReportDialog("openFolder");    
+        }
     }
 
 }

es.icarto.gvsig.siga/src/es/icarto/gvsig/commons/queries/ReportValidation.java
@@ -1,0 +1,28 @@
+package es.icarto.gvsig.commons.queries;
+
+import javax.swing.table.DefaultTableModel;
+
+import com.iver.andami.PluginServices;
+
+public class ReportValidation {
+
+    // excel libs have a maximum number of rows allowed.
+    // Also too many rows can provoke memory errors creating the pdf
+    private static final int MAX_ROWS_ALLOWED = 65000;
+
+    public ReportValidationResult afterGetResults(DefaultTableModel table) {
+        String msg = "";
+        final int rowCount = table.getRowCount();
+        
+        if (rowCount == 0) {
+            msg = PluginServices.getText(this, "queryWithoutResults_msg");
+        }
+
+        if (rowCount > MAX_ROWS_ALLOWED) {
+            msg = PluginServices.getText(this, "queryWithTooMuchResults_msg");
+        }
+        
+        return new ReportValidationResult(msg);
+    }
+
+}

es.icarto.gvsig.siga/src/es/icarto/gvsig/commons/queries/ReportValidationResult.java
@@ -1,0 +1,21 @@
+package es.icarto.gvsig.commons.queries;
+
+import es.icarto.gvsig.commons.utils.StrUtils;
+
+public class ReportValidationResult {
+    
+    private final String msg;
+
+    public ReportValidationResult(String msg) {
+        this.msg = msg;
+    }
+    
+    public boolean isError() {
+        return !StrUtils.isEmptyString(msg);
+    }
+    
+    public String getMsg( ) {
+        return this.msg;
+    }
+
+}

extGEX/src/es/icarto/gvsig/extgex/queries/QueriesPanel.java
@@ -18,12 +18,13 @@ import org.apache.log4j.Logger;
 
 import com.iver.andami.PluginServices;
 import com.iver.andami.messages.NotificationManager;
-import com.iver.andami.ui.mdiManager.WindowInfo;
 
 import es.icarto.gvsig.commons.db.ConnectionWrapper;
 import es.icarto.gvsig.commons.queries.CustomiceDialog;
 import es.icarto.gvsig.commons.queries.FinalActions;
 import es.icarto.gvsig.commons.queries.QueriesWidget;
+import es.icarto.gvsig.commons.queries.ReportValidation;
+import es.icarto.gvsig.commons.queries.ReportValidationResult;
 import es.icarto.gvsig.commons.queries.Utils;
 import es.icarto.gvsig.commons.queries.ValidatableForm;
 import es.icarto.gvsig.commons.utils.Field;
@@ -167,13 +168,11 @@ public class QueriesPanel extends ValidatableForm implements ActionListener {
         ResultTableModel result = new ResultTableModel(queryCode, queryDescription, queryTitle, querySubtitle);
         result.setQueryFilters(queryFilters);
         con.execute(query, result);
+        ReportValidationResult reportValidationResult = new ReportValidation().afterGetResults(result);
 
         File file = queriesOuputWidget.to(result, queryFilters);
-        if (file != null) {
-            FinalActions finalActions = new FinalActions(result.getRowCount() == 0, file);
-            finalActions.openReport();
-        }
-
+        FinalActions finalActions = new FinalActions(file, reportValidationResult);
+        finalActions.openReport();
     }
 
     private void popToDestination(List<Field> fields, String key, CustomiceDialog<Field> customiceDialog) {

extGIA/src/es/icarto/gvsig/extgia/consultas/CaracteristicasQueries.java
@@ -7,43 +7,26 @@ import es.icarto.gvsig.extgia.preferences.Elements;
 public class CaracteristicasQueries {
 
     public static String getPDFCaracteristicasQuery(String element, ConsultasFilters<Field> filters) {
-    String query = "";    
-    switch (Elements.valueOf(element)) {
-        case Senhalizacion_Vertical:
-            query = "SELECT gid, " + ConsultasFieldNames.getPDFCaracteristicasFieldNames(element)
-                    + CaracteristicasQueries.getFromClauseCaracteristicas(element);
-            if (!filters.getWhereClauseByLocationWidgets().isEmpty()) {
+        String query = "SELECT gid, " + ConsultasFieldNames.getPDFCaracteristicasFieldNames(element)
+                + CaracteristicasQueries.getFromClauseCaracteristicas(element);
+        if (!filters.getWhereClauseByLocationWidgets().isEmpty()) {
             query = query + filters.getWhereClauseByLocationWidgets();
-            }
-            if (!filters.getWhereClauseBySelectedRecordsOnly("sub", Elements.valueOf(element).pk).isEmpty()) {
-            query = query + filters.getWhereClauseBySelectedRecordsOnly("sub", Elements.valueOf(element).pk);
-            query = query.replace("sub.", "");
-            }
-            return query + " ORDER BY el.id_elemento_senhalizacion";
-        
-        default:
-            query = "SELECT gid, " + ConsultasFieldNames.getPDFCaracteristicasFieldNames(element)
-            + CaracteristicasQueries.getFromClauseCaracteristicas(element);
-            if (!filters.getWhereClauseByLocationWidgets().isEmpty()) {
-            query = query + filters.getWhereClauseByLocationWidgets();
-            }
-            if (!filters.getWhereClauseBySelectedRecordsOnly("sub", Elements.valueOf(element).pk).isEmpty()) {
-            query = query + filters.getWhereClauseBySelectedRecordsOnly("sub", Elements.valueOf(element).pk);
-            query = query.replace("sub.", "");
-            }
-            return query + " ORDER BY gid";
-
         }
+        if (!filters.getWhereClauseBySelectedRecordsOnly("sub", Elements.valueOf(element).pk).isEmpty()) {
+            query = query + filters.getWhereClauseBySelectedRecordsOnly("sub", Elements.valueOf(element).pk);
+            query = query.replace("sub.", "");
+        }
+        if (Elements.valueOf(element).equals(Elements.Senhalizacion_Vertical)) {
+            query += " ORDER BY el.id_elemento_senhalizacion";
+        } else {
+            query += " ORDER BY gid";
+        }
+        return query;
     }
 
     public static String getReconocimientosTrabajosQuery(String element, ConsultasFilters<Field> filters,
             String fields, String elementId, String tipoConsulta) {
-        String query = "";
-        if (element.equalsIgnoreCase("senhalizacion_vertical")) {
-            query = "SELECT " + fields + getFromClauseReconocimientosTrabajos(element, elementId, tipoConsulta, filters);
-        } else {
-            query = "SELECT " + fields + getFromClauseReconocimientosTrabajos(element, elementId, tipoConsulta, filters);
-        }
+        String query = "SELECT " + fields + getFromClauseReconocimientosTrabajos(element, elementId, tipoConsulta, filters);
 
         if (!filters.getWhereClauseByLocationWidgets().isEmpty()) {
             if (filters.getUltimos()) {
@@ -150,12 +133,12 @@ public class CaracteristicasQueries {
         case Centros_Mando:
         case Pretiles:
         case Barrera_Metalica:
+        case Valla_Cierre:
             return getLocalizationTablesWithSentido();
 
         case Firme:
             return getFirmeLocalizationTables();
 
-        case Valla_Cierre:
         case Taludes:
         case Muros:
         case Lineas_Suministro:

extGIA/src/es/icarto/gvsig/extgia/consultas/Composite.java
@@ -8,6 +8,7 @@ import javax.swing.JFileChooser;
 
 import es.icarto.gvsig.commons.queries.Component;
 import es.icarto.gvsig.commons.queries.FinalActions;
+import es.icarto.gvsig.commons.queries.ReportValidationResult;
 import es.icarto.gvsig.commons.utils.Field;
 import es.icarto.gvsig.navtableforms.ormlite.domainvalues.KeyValue;
 
@@ -55,7 +56,7 @@ public class Composite implements Component {
 
     @Override
     public void finalActions() {
-	FinalActions finalActions = new FinalActions(false, outputPath);
+	FinalActions finalActions = new FinalActions(outputPath, new ReportValidationResult(""));
 	finalActions.openFolder();
     }
 

commit 94986be3a8e00436794858dc5408fd3f127a0318

Pre create an empty validation result in 'Consultas' to avoid NullPointerException
Fixes bug introduced in: ace155ffda4bd9
refs #2154

extGIA/src/es/icarto/gvsig/extgia/consultas/Leaf.java
@@ -75,6 +75,7 @@ public class Leaf implements Component {
     @Override
     public void generateReportFile() {
 	QueryType tipo = getTipo();
+	reportValidationResult = new ReportValidationResult("");
 	if (tipo == QueryType.TRABAJOS_AGREGADOS) {
 	    if (pdf) {
 		createPdfReportAgregados(outputFile.getAbsolutePath(), element,